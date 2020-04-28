9beb7f1d9e1e0d776a6b71a8a6f771b9944645e3
Jesús Pérez-Martín <jperezmartin90@gmail.com>
2020-04-28 05:13:43 +0000

    updates models

--- models/sadness.py
+++ models/sadness.py
@@ -7,9 +7,9 @@ from sklearn.utils.multiclass import unique_labels
 class SadnessTweetClassifier(BaseEstimator, ClassifierMixin): 
   def __init__(self, demo_param='demo'):
     self.demo_param = demo_param
+    self.classifier = MultinomialNB()
 
   def fit(self, X, y):
-
     # Check that X and y have correct shape
     X, y = check_X_y(X, y, accept_sparse=True)
     # Store the classes seen during fit
@@ -18,19 +18,27 @@ class SadnessTweetClassifier(BaseEstimator, ClassifierMixin):
     self.X_ = X
     self.y_ = y
 
+    self.classifier.fit(X, y)
+
     # Return the classifier
     return self
 
   def predict_proba(self, X):
-    return np.random.rand(X.shape[0],3)
+    # Check is fit had been called
+    check_is_fitted(self)
+    # Input validation
+    X = check_array(X, accept_sparse=True)
+
+    # return np.random.rand(X.shape[0],3)
+    return self.classifier.predict_proba(X)
 
   def predict(self, X):
-
     # Check is fit had been called
     check_is_fitted(self)
 
     # Input validation
     X = check_array(X)
 
-    closest = np.argmin(euclidean_distances(X, self.X_), axis=1)
-    return self.y_[closest]
+    # closest = np.argmin(euclidean_distances(X, self.X_), axis=1)
+    # return self.y_[closest]
+    return self.classifier.predict(X)

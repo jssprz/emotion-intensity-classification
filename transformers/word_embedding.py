65d1068bdc914d28d3670ec0434c71af4fa0caf1
Jesús <jperezmartin90@gmail.com>
2020-05-16 04:47:49 +0000

    loads wordvectors before initialize word_embedding transformer

--- transformers/word_embedding.py
+++ transformers/word_embedding.py
@@ -4,48 +4,39 @@ from gensim.models.keyedvectors import KeyedVectors
 import nltk
 nltk.download('punkt')
 from nltk.tokenize import word_tokenize
+from keras.models import Sequential
+from keras.layers import LSTM, Dense, Dropout, Masking, Embedding
 
 class WordEmbeddingTransformer(BaseEstimator, TransformerMixin):
-  def __init__(self, wordvectors_path, use_maximum=False):
-    self.use_maximum = use_maximum
-    if wordvectors_path.split('.')[-1] == 'vec': 
-      count = 100000
-      self.wordvectors = KeyedVectors.load_word2vec_format(wordvectors_path, limit=count)
-    elif wordvectors_path.split('.')[-1] == 'txt':
-      self.wordvectors = {}
-      with open(wordvectors_path) as f:
-        for line in f:
-          s = line.strip().split(' ')
-          if len(s) == 201:
-            self.wordvectors[s[0]] = np.array(s[1:], dtype=float)
+  def __init__(self, wordvectors, aggregation='None'):
+    self.wordvectors = wordvectors
+    self.aggregation = aggregation
 
-  def get_relevant_chars(self, tweet):
-    num_hashtags = tweet.count('#')
-    num_exclamations = tweet.count('!')
-    num_interrogations = tweet.count('?')
-    return [num_hashtags, num_exclamations, num_interrogations]
-
-  def get_tweet_emmbeding(self, tweet):
+  def get_embeddings(self, tweet):
     word_embeddings = []
-    for t in tweet.split(' '):
-        try:
-          vec = self.wordvectors[t]
-        except:
-            # print('error with word "{}" reduced to "{}"'.format(self.vocab[t], s))
-          pass
-        else:
-          word_embeddings.append(vec)
+    for t in word_tokenize(tweet):
+      try:
+        vec = self.wordvectors[t]
+      except:
+          # print('error with word "{}" reduced to "{}"'.format(self.vocab[t], s))
+        pass
+      else:
+        word_embeddings.append(vec)
+    return word_embeddings
 
+  def agregate(self, word_embeddings):
     if not len(word_embeddings):
       return np.zeros_like(self.wordvectors['a'])
-    if self.use_maximum:
+    if self.aggregation == 'max':
       return np.max(np.array(word_embeddings), axis=0)
-    return np.mean(np.array(word_embeddings), axis=0)
+    if self.aggregation == 'mean':
+      return np.mean(np.array(word_embeddings), axis=0)
+    return word_embeddings
 
   def transform(self, X, y=None):
     embeddigs = []
     for tweet in X:
-      embeddigs.append(self.get_tweet_emmbeding(tweet))
+      embeddigs.append(self.agregate(self.get_embeddings(tweet)))
     return np.array(embeddigs)
     # chars = []
     # for tweet in X:
